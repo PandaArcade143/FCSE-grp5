ddac0d9cd5780b0fb0de769e118f74bff038eb95
Loong <105075744+tebii@users.noreply.github.com>
2025-03-25 17:33:46 +0800

    Create HDBOfficer.java

---
+++ src/entity/HDBOfficer.java
@@ -0,0 +1,9 @@
+package entity;
+
+public class HDBOfficer extends User{
+
+	public HDBOfficer(String name, String nric, int age, String maritalStatus, String password) {
+		super(name, nric, age, maritalStatus, password);
+	}
+
+}
