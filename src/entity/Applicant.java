c73839230d0ef883b612a234fe0f454a58aa6823
Loong <105075744+tebii@users.noreply.github.com>
2025-03-25 17:33:52 +0800

    Create Applicant.java

---
+++ src/entity/Applicant.java
@@ -0,0 +1,16 @@
+package entity;
+
+public class Applicant extends User{
+
+	public Applicant(String name, String nric, int age, String maritalStatus, String password) {
+		super(name, nric, age, maritalStatus, password);
+	}
+	
+	
+	
+	
+	// Setter
+	
+
+
+}
