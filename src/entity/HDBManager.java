19c4764653bb80854baa88fbbdd3fc411278deb5
Loong <105075744+tebii@users.noreply.github.com>
2025-03-25 17:33:50 +0800

    Create HDBManager.java

---
+++ src/entity/HDBManager.java
@@ -0,0 +1,9 @@
+package entity;
+
+public class HDBManager extends Applicant {
+
+	public HDBManager(String name, String nric, int age, String maritalStatus, String password) {
+		super(name, nric, age, maritalStatus, password);
+	}
+
+}
