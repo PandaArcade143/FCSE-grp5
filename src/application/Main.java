b6c23ac02d12059efcc9a5ff40b57afbfdac06bc
Loong <105075744+tebii@users.noreply.github.com>
2025-03-25 17:33:27 +0800

    Add main.java

---
+++ src/application/Main.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.util.List;
+
+import entity.Applicant;
+import entity.HDBManager;
+import entity.HDBOfficer;
+import helpers.DataManager;
+
+public class Main {
+	public static void main(String[] args) {
+		// Loads the lists on startup
+		List<Applicant> applicantList = DataManager.loadUsers("data/ApplicantList.csv", Applicant.class);
+		List<HDBManager> managerList = DataManager.loadUsers("data/ManagerList.csv", HDBManager.class);
+		List<HDBOfficer> officerList = DataManager.loadUsers("data/OfficerList.csv", HDBOfficer.class);
+
+		for (Applicant a : applicantList) {
+			System.out.println(a.getName());
+		}
+	}
+
+}
