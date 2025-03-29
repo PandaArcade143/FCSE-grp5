6ad4cfcd13455d0462cbd247d160218d0e84a71a
Loong <105075744+tebii@users.noreply.github.com>
2025-03-30 03:28:23 +0800

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.List;
 
+import boundary.ApplicantUI;
+import control.AuthController;
 import entity.Applicant;
 import entity.HDBManager;
 import entity.HDBOfficer;
@@ -10,6 +12,7 @@ import helpers.DataManager;
 public class Main {
 	public static void main(String[] args) {
 		// Loads the lists on startup
+		// this should go into the controller
 		List<Applicant> applicantList = DataManager.loadUsers("data/ApplicantList.csv", Applicant.class);
 		List<HDBManager> managerList = DataManager.loadUsers("data/ManagerList.csv", HDBManager.class);
 		List<HDBOfficer> officerList = DataManager.loadUsers("data/OfficerList.csv", HDBOfficer.class);
@@ -17,6 +20,8 @@ public class Main {
 		for (Applicant a : applicantList) {
 			System.out.println(a.getName());
 		}
+		AuthController authcontroller = new AuthController();
+		
 	}
 
 }
