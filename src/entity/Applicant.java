e8a8f993595f217ed23995c0ad1e3ac7e9771310
Loong <105075744+tebii@users.noreply.github.com>
2025-03-30 03:28:01 +0800

    Update Applicant.java

--- src/entity/Applicant.java
+++ src/entity/Applicant.java
@@ -5,6 +5,10 @@ public class Applicant extends User{
 	public Applicant(String name, String nric, int age, String maritalStatus, String password) {
 		super(name, nric, age, maritalStatus, password);
 	}
+
+	public String getRole() {
+		return "Applicant";
+	}
 	
 	
 	
