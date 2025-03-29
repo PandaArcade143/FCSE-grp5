604d6b9b88f6032205b044e2268f17a309719d44
Loong <105075744+tebii@users.noreply.github.com>
2025-03-30 03:28:03 +0800

    Update HDBManager.java

--- src/entity/HDBManager.java
+++ src/entity/HDBManager.java
@@ -5,5 +5,10 @@ public class HDBManager extends Applicant {
 	public HDBManager(String name, String nric, int age, String maritalStatus, String password) {
 		super(name, nric, age, maritalStatus, password);
 	}
+	
+	public String getRole() {
+		return "HDBManager";
+		
+	}
 
 }
