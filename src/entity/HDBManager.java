510d7f947268cfc096490a7f10f99d0daecb5e97
Loong <105075744+tebii@users.noreply.github.com>
2025-03-30 17:37:10 +0800

    Update HDBManager.java

--- src/entity/HDBManager.java
+++ src/entity/HDBManager.java
@@ -1,6 +1,6 @@
 package entity;
 
-public class HDBManager extends Applicant {
+public class HDBManager extends User{
 
 	public HDBManager(String name, String nric, int age, String maritalStatus, String password) {
 		super(name, nric, age, maritalStatus, password);
