586ce97325424b62abc7b67219e8b371d2280891
Loong <105075744+tebii@users.noreply.github.com>
2025-03-30 17:37:07 +0800

    Update Applicant.java

--- src/entity/Applicant.java
+++ src/entity/Applicant.java
@@ -1,6 +1,8 @@
 package entity;
 
 public class Applicant extends User{
+	Project project;
+	String applicationStatus;
 
 	public Applicant(String name, String nric, int age, String maritalStatus, String password) {
 		super(name, nric, age, maritalStatus, password);
@@ -8,6 +10,14 @@ public class Applicant extends User{
 
 	public String getRole() {
 		return "Applicant";
+	}
+
+	public Project getAppliedProject() {
+		return project;
+	}
+
+	public String getApplicationStatus() {
+		return applicationStatus;
 	}
 	
 	
