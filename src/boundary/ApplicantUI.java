de0ee8a20c3a1ff526fc256d2663e67c70a80f4a
PandaArcade143 <zhuoxungames@gmail.com>
2025-04-14 18:16:52 +0800

    HDBOfficer

--- src/boundary/ApplicantUI.java
+++ src/boundary/ApplicantUI.java
@@ -11,7 +11,6 @@ import java.util.Scanner;
 public class ApplicantUI {
     ProjectController projectController = new ProjectController();
     InquiryController inquiryController = new InquiryController();
-    Scanner scanner = new Scanner(System.in);
 
     // Display menu for Applicant user
     public void showMenu(Applicant applicant) {
@@ -29,162 +28,172 @@ public class ApplicantUI {
         System.out.println("4. Withdraw my application");
         System.out.println("5. Submit an inquiry");
         System.out.println("6. View and manage inquiries");
+        System.out.println("7. Quit");
         System.out.print("Select an option: ");
         
-        int choice = scanner.nextInt(); // Input choice of option
-        switch (choice) {
-            // Displays list of available projects
-            case 1:// Loops through list of projects and prints out their names
-                for (Project project : projectList) {
-                    System.out.println(project.getName());
-                }
-                break;
-
-            // Applicant applies for a project
-            case 2:
-                System.out.println("Enter the name of the project you wish to apply: ");
-                String projectName = scanner.nextLine(); // Applicant inputs the name of the project they wish to apply
-                boolean projectApplied = projectController.applyToProject(applicant, projectName); // Applies to the project and gets whether applicant is eligible for application
-                
-                // Displays message depending on whether project is successfully applied
-                if (projectApplied) {
-                    System.out.println("Project successfully applied!");
-                } else {
-                    System.out.println("Project is unable to be applied to.");
-                }
-                break;
-
-            // Displays applicant's application status
-            case 3:
-                String applicationStatus = applicant.getApplicationStatus();
-                System.out.print("Current application status: ");
-                System.out.println(applicationStatus);
-                
-                /////////////////////////////////////////////////////////////////////////
-                // Is applicant supposed to be able to book through HDB Officer here???//
-                /////////////////////////////////////////////////////////////////////////
-
-                break;
-
-            // Withdraw application of project
-            //////////////////////
-            /// Not sure how do///
-            /// //////////////////
-            case 4:
-                //withdrawApplication();
-                break;
-
-            // Submit applicant's inquiry
-            case 5:
-                // Display available projects
-                for (int i=1; i<=projectList.size(); i++) {
-                    System.out.print(i);
-                    System.out.print(". ");
-                    System.out.println(projectList.get(i-1).getName());
-                }
-
-                // Applicant chooses which project to inquire
-                System.out.println("Which project do you wish to inquire about: ");
-                int projectIndex = scanner.nextInt() - 1;
-                if (projectIndex < 0 || projectIndex > projectList.size()) {
-                    System.out.println("Project does not exist.");
+        while (true) {
+        	int choice = scanner.nextInt(); // Input choice of option
+        	//Quit the program
+        	if (choice == 7) {
+            	System.out.println("Quit successful.");
+            	break;	
+        	}
+        	switch (choice) {
+                // Displays list of available projects
+                case 1:// Loops through list of projects and prints out their names
+                	System.out.println("Projects available:");
+                	for (Project project : projectList) {
+                        System.out.println(project.getName());
+                    }
                     break;
-                }
-                Project project = projectList.get(projectIndex);
 
-                // Applicant enters a subject for inquiry
-                System.out.println("What is the subject for your inquiry: ");
-                String subject = scanner.nextLine();
+                // Applicant applies for a project
+                case 2:
+                    System.out.println("Enter the name of the project you wish to apply: ");
+                    String projectName = scanner.nextLine(); // Applicant inputs the name of the project they wish to apply
+                    boolean projectApplied = projectController.applyToProject(applicant, projectName); // Applies to the project and gets whether applicant is eligible for application
+                    
+                    // Displays message depending on whether project is successfully applied
+                    if (projectApplied) {
+                        System.out.println("Project successfully applied!");
+                    } else {
+                        System.out.println("Project is unable to be applied to.");
+                    }
+                    break;
 
-                // Applicant enters message for inquiry
-                System.out.println("What is your inquiry: ");
-                String message = scanner.nextLine();
+                // Displays applicant's application status
+                case 3:
+                    String applicationStatus = applicant.getApplicationStatus();
+                    System.out.print("Current application status: ");
+                    System.out.println(applicationStatus);
+                    
+                    /////////////////////////////////////////////////////////////////////////
+                    // Is applicant supposed to be able to book through HDB Officer here???//
+                    /////////////////////////////////////////////////////////////////////////
 
-                // Create and add inquiry
-                InquiryController.createInquiry(applicant.getNRIC(), subject, project, message);
-                break;
+                    break;
 
-            // View/Edit/Delete applicant's inquiries
-            case 6:
-                System.out.println("Inquiry Menu");
-                System.out.println("1. View");
-                System.out.println("2. Edit");
-                System.out.println("3. Delete");
-                System.out.println("Select an option: ");
+                // Withdraw application of project
+                //////////////////////
+                /// Not sure how do///
+                /// //////////////////
+                case 4:
+                    //withdrawApplication();
+                    break;
 
-                int option = scanner.nextInt();
-                switch (option) {
-                    // View applicant's inquiries
-                    case 1:
-                        for (int i=1; i<=inquiryList.size(); i++) {
-                            System.out.print(i);
-                            System.out.print(". ");
-                            System.out.print(inquiryList.get(i-1).getSubject());
-                            System.out.print(": ");
-                            System.out.println(inquiryList.get(i-1).getMessage());
-                        }
+                // Submit applicant's inquiry
+                case 5:
+                    // Display available projects
+                    for (int i=1; i<=projectList.size(); i++) {
+                        System.out.print(i);
+                        System.out.print(". ");
+                        System.out.println(projectList.get(i-1).getName());
+                    }
+
+                    // Applicant chooses which project to inquire
+                    System.out.println("Which project do you wish to inquire about: ");
+                    int projectIndex = scanner.nextInt() - 1;
+                    if (projectIndex < 0 || projectIndex > projectList.size()) {
+                        System.out.println("Project does not exist.");
                         break;
+                    }
+                    Project project = projectList.get(projectIndex);
 
-                    // Edit applicant's inquiries
-                    case 2:
-                        // Display applicant's inquiries
-                        for (int i=1; i<=inquiryList.size(); i++) {
-                            System.out.print(i);
-                            System.out.print(". ");
-                            System.out.print(inquiryList.get(i-1).getSubject());
-                            System.out.print(": ");
-                            System.out.println(inquiryList.get(i-1).getMessage());
-                        }
+                    // Applicant enters a subject for inquiry
+                    System.out.println("What is the subject for your inquiry: ");
+                    String subject = scanner.nextLine();
 
-                        // Applicant chooses inquiry to edit
-                        System.out.println("Which inquiry do you wish to edit: ");
-                        inquiryIndex = scanner.nextInt() - 1;
-                        if (inquiryIndex < 0 || inquiryIndex > inquiryList.size()) {
-                            System.out.println("Inquiry does not exist.");
+                    // Applicant enters message for inquiry
+                    System.out.println("What is your inquiry: ");
+                    String message = scanner.nextLine();
+
+                    // Create and add inquiry
+                    InquiryController.createInquiry(applicant.getNRIC(), subject, project, message);
+                    break;
+
+                // View/Edit/Delete applicant's inquiries
+                case 6:
+                    System.out.println("Inquiry Menu");
+                    System.out.println("1. View");
+                    System.out.println("2. Edit");
+                    System.out.println("3. Delete");
+                    System.out.println("Select an option: ");
+
+                    int option = scanner.nextInt();
+                    switch (option) {
+                        // View applicant's inquiries
+                        case 1:
+                            for (int i=1; i<=inquiryList.size(); i++) {
+                                System.out.print(i);
+                                System.out.print(". ");
+                                System.out.print(inquiryList.get(i-1).getSubject());
+                                System.out.print(": ");
+                                System.out.println(inquiryList.get(i-1).getMessage());
+                            }
                             break;
-                        }
-                        inquiry = inquiryList.get(inquiryIndex);
 
-                        // Applicant enters new message
-                        System.out.println("What would you like to change your inquiry to: ");
-                        String newMessage = scanner.nextLine();
+                        // Edit applicant's inquiries
+                        case 2:
+                            // Display applicant's inquiries
+                            for (int i=1; i<=inquiryList.size(); i++) {
+                                System.out.print(i);
+                                System.out.print(". ");
+                                System.out.print(inquiryList.get(i-1).getSubject());
+                                System.out.print(": ");
+                                System.out.println(inquiryList.get(i-1).getMessage());
+                            }
 
-                        // Edit inquiry
-                        InquiryController.editInquiry(applicant.getNRIC(), inquiry, newMessage);
-                        break;
+                            // Applicant chooses inquiry to edit
+                            System.out.println("Which inquiry do you wish to edit: ");
+                            inquiryIndex = scanner.nextInt() - 1;
+                            if (inquiryIndex < 0 || inquiryIndex > inquiryList.size()) {
+                                System.out.println("Inquiry does not exist.");
+                                break;
+                            }
+                            inquiry = inquiryList.get(inquiryIndex);
 
-                    // Delete applicant's inquiry
-                    case 3:
-                        // Display applicant's inquiries
-                        for (int i=1; i<=inquiryList.size(); i++) {
-                            System.out.print(i);
-                            System.out.print(". ");
-                            System.out.print(inquiryList.get(i-1).getSubject());
-                            System.out.print(": ");
-                            System.out.println(inquiryList.get(i-1).getMessage());
-                        }
+                            // Applicant enters new message
+                            System.out.println("What would you like to change your inquiry to: ");
+                            String newMessage = scanner.nextLine();
 
-                        // Applicant chooses inquiry to edit
-                        System.out.println("Which inquiry do you wish to edit: ");
-                        inquiryIndex = scanner.nextInt() - 1;
-                        if (inquiryIndex < 0 || inquiryIndex > inquiryList.size()) {
-                            System.out.println("Inquiry does not exist.");
+                            // Edit inquiry
+                            InquiryController.editInquiry(applicant.getNRIC(), inquiry, newMessage);
                             break;
-                        }
-                        inquiry = inquiryList.get(inquiryIndex);
 
-                        // Delete inquiry
-                        InquiryController.deleteInquiry(applicant.getNRIC(), inquiry);
-                        break;
+                        // Delete applicant's inquiry
+                        case 3:
+                            // Display applicant's inquiries
+                            for (int i=1; i<=inquiryList.size(); i++) {
+                                System.out.print(i);
+                                System.out.print(". ");
+                                System.out.print(inquiryList.get(i-1).getSubject());
+                                System.out.print(": ");
+                                System.out.println(inquiryList.get(i-1).getMessage());
+                            }
 
-                    default:
-                        System.out.println("Invalid option.");
-                }
-                break;
+                            // Applicant chooses inquiry to edit
+                            System.out.println("Which inquiry do you wish to edit: ");
+                            inquiryIndex = scanner.nextInt() - 1;
+                            if (inquiryIndex < 0 || inquiryIndex > inquiryList.size()) {
+                                System.out.println("Inquiry does not exist.");
+                                break;
+                            }
+                            inquiry = inquiryList.get(inquiryIndex);
 
-            default:
-                System.out.println("Invalid option.");
+                            // Delete inquiry
+                            InquiryController.deleteInquiry(applicant.getNRIC(), inquiry);
+                            break;
+
+                        default:
+                            System.out.println("Invalid option.");
+                    }
+                    break;
+                	
+                default:
+                    System.out.println("Invalid option.");
+            }
         }
+        
         scanner.close();
     }
 }
--- src/boundary/HDBOfficerUI.java
+++ src/boundary/HDBOfficerUI.java
@@ -1,42 +1,124 @@
 package boundary;
 
-import java.util.Scanner;
+import java.util.*;
+
+import control.InquiryController;
+import control.ProjectController;
+import entity.HDBOfficer;
+import entity.Inquiry;
+import entity.Project;
 
 public class HDBOfficerUI {
+	ProjectController projectController = new ProjectController();
+    InquiryController inquiryController = new InquiryController();
     
-    public void showMenu() {
+    
+    public void showMenu(HDBOfficer officer) {
         Scanner scanner = new Scanner(System.in);
-
+        List<Project> projectList = projectController.getAvailableProjects(officer); // Fetches list of available projects
+        Project registeredProject = officer.getRegisteredProjects();
+        
         // Display menu
         System.out.println("HDB Officer Menu:");
-        System.out.println("1. Register for a project");
-        System.out.println("2. View registration status for projects");
-        System.out.println("3. View project details");
-        System.out.println("4. Respond to enquiries");
-        System.out.println("5. Update BTO status");
-        System.out.println("6. Generate applicant receipt");
+        System.out.println("1. View available BTO projects");
+        System.out.println("2. Register for a project");
+        System.out.println("3. View registration status for projects");
+        System.out.println("4. View project details");
+        System.out.println("5. Respond to enquiries");
+        System.out.println("6. Update BTO status");
+        System.out.println("7. Generate applicant receipt");
+        System.out.println("8. Quit");
         System.out.print("Select an option: ");
         
-        int choice = scanner.nextInt();
-        switch (choice) {
-            case 1:
-                registerForProject();
-                break;
-            case 2:
-                viewRegistrationStatus();
-                break;
-            case 3:
-                viewProjectDetails();
-                break;
-            case 4:
-                respondToEnquiries();
-            case 5:
-                updateBTOStatus();
-            case 6:
-                generateReceipt();
-            default:
-                System.out.println("Invalid option.");
+        while (true) {
+        	int choice = scanner.nextInt();
+        	//Quit the program
+        	if (choice == 8) {
+            	System.out.println("Quit successful.");
+            	break;	
+        	}
+            switch (choice) {
+            	// Display list of available projects
+                case 1:// Loops through list of projects and prints out their names
+                	System.out.println("Projects available:");
+                	for (Project project : projectList) {
+                        System.out.println(project.getName());
+                    }
+                    break;
+                    
+                // HDB officer registers for a project
+                case 2:
+                	System.out.println("Enter the name of the project you wish to register for: ");
+                    String projectName = scanner.nextLine(); // Applicant inputs the name of the project they wish to apply
+                    Date currDate = new Date();
+                    for (Project project : projectList) {
+                        if (project.getName().compareToIgnoreCase(projectName) == 0) {
+                        	registeredProject = project;
+                        }
+                    }
+                    // Displays message depending on whether project is successfully applied
+                    if (officer.isEligibleForRegistration(registeredProject) == false) {
+                        System.out.println("Project is unable to be registered for as you are already applying as an applicant.");
+                    } else if (projectName == officer.getRegisteredProjects().getName()){
+                        System.out.println("You have already registered for this project.");
+                    } else if (officer.getRegisteredProjects() != null && !officer.getRegisteredProjects().getOpenDate().after(currDate) && !officer.getRegisteredProjects().getCloseDate().before(currDate)) {
+                    	System.out.println("You have already registered for another project.");
+                    } else if (registeredProject.getOfficerSlot() == registeredProject.getOfficers().size()){
+                    	System.out.println("No free officer slots left for this project.");
+                    } else{
+                    	System.out.println("Project successfully registered!");
+                    	//NOT SURE HOW TO REGISTER HDBOFFICER
+                    	//registeredProject.addTemporaryOfficer();
+                    }
+                    break;
+                case 3:
+                	System.out.println("Registration status:");
+                	//WHAT IS THE KEY VALUE IN THE ARGUMENT LIST??
+                	//System.out.println(officer.getRegistrationStatus())
+                    break;
+                case 4:
+                	System.out.println("Project Name: " + registeredProject.getName());
+                	System.out.println("Neighborhood: " + registeredProject.getLocation());
+                	System.out.println("Flat types and total number of units for corresponding types:");
+                	for (Map.Entry<String, Integer> pair : registeredProject.getFlatTypeTotal().entrySet()) {
+                       System.out.print("	Flat type: " + pair.getKey() + ", total number of units: " + pair.getValue());
+                    }
+                	System.out.println("Flat types and available number of units left for corresponding types:");
+                	for (Map.Entry<String, Integer> pair : registeredProject.getFlatTypeAvailable().entrySet()) {
+                       System.out.print("	Flat type: " + pair.getKey() + ", available number of units left: " + pair.getValue());
+                    }
+                	System.out.println("Flat types and prices for corresponding types:");
+                	for (Map.Entry<String, Integer> pair : registeredProject.getFlatPrices().entrySet()) {
+                       System.out.print("	Flat type: " + pair.getKey() + ", selling price: " + pair.getValue());
+                    }
+                	System.out.println("Application opening date: " + registeredProject.getOpenDate());
+                	System.out.println("Application closing date: " + registeredProject.getCloseDate());
+                	System.out.println("Manager of project: " + registeredProject.getManager());
+                	System.out.println("Officer slots for project: " + registeredProject.getOfficerSlot());
+                	System.out.println("Officers of project: ");
+                	for (String hdbofficer: registeredProject.getOfficers()) {
+                		System.out.println("	" + hdbofficer);
+                    }
+                case 5:
+                	System.out.print("Inquiry ID and messages:");
+                	for (Inquiry inquiry: InquiryController.viewInquiries(registeredProject)) {
+                		System.out.println(inquiry.getInquiryId() + ": " + inquiry.getMessage());
+                    }
+                	System.out.print("Enter Inquiry ID to respond to:");
+                	String inquiryID = scanner.nextLine();
+                	System.out.print("Enter reply message: ");
+                	String replyMessage = scanner.nextLine();
+                	InquiryController.replyToInquiry(inquiryID, replyMessage);
+                case 6:
+                	Map<String, Integer> flatTypeAvailable = registeredProject.getFlatTypeAvailable();
+
+                	System.out.print("Enter flat type to update: ");
+                	String flatType = scanner.nextLine();
+                default:
+                    System.out.println("Invalid option.");
+            }
         }
+        
         
         scanner.close();
     }
