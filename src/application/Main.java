2d35045eccb1b1555b31e316a7833bc9f56b85fb
Loong <105075744+tebii@users.noreply.github.com>
2025-04-16 22:33:15 +0800

    Update Main.java
    now it only launches the loginmenu

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,16 +13,8 @@ import helpers.DataManager;
 
 public class Main {
 	public static void main(String[] args) {
-		// Loads the lists on startup
-		// this should go into the controller
-		List<Applicant> applicantList = DataManager.loadUsers("data/ApplicantList.csv", Applicant.class);
-		List<HDBManager> managerList = DataManager.loadUsers("data/ManagerList.csv", HDBManager.class);
-		List<HDBOfficer> officerList = DataManager.loadUsers("data/OfficerList.csv", HDBOfficer.class);
-
-		for (Applicant a : applicantList) {
-			System.out.println(a.getName());
-		}
-		AuthController authcontroller = new AuthController();
+		LoginMenu menu = new LoginMenu();
+		menu.showMenu(DataManager.getApplicants(), DataManager.getManagers(), DataManager.getOfficers());
 	}
 
 }
