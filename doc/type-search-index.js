7e1474b4254ad795113570ce30723cb4a1164c4d
Loong <105075744+tebii@users.noreply.github.com>
2025-04-24 23:57:06 +0800

    javadoc

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"boundary","l":"HDBManagerUI"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"boundary","l":"HDBStaffInt"}];updateSearchResults();
